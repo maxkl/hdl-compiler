029e0b31f87e317e8370f764165f435ba3bd05b7
maxkl <max@maxkl.de>
2019-10-10 18:42:41 +0200

    Only accept a single input file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 mod shared;
 mod frontend;
-mod linker;
 mod backend;
 
 use std::path::Path;
@@ -11,7 +10,6 @@ use clap::{App, Arg};
 use derive_more::Display;
 
 use crate::shared::intermediate::Intermediate;
-use crate::linker::Linker;
 use crate::shared::error;
 
 const FRONTENDS: [(&str, fn(&Path) -> frontend::Result); 1] = [
@@ -31,12 +29,8 @@ const INPUT_FILE_TYPES: [(&str, &str); 2] = [
 pub enum ErrorKind {
     #[display(fmt = "failed to compile {}", _0)]
     Compile(String),
-    #[display(fmt = "failed to link intermediate files")]
-    Linker,
     #[display(fmt = "error in backend")]
     Backend,
-    #[display(fmt = "-o specified for multiple output files")]
-    MultipleOutputsFileSpecified,
     #[display(fmt = "{}: file format not recognized", _0)]
     FileFormatNotRecognized(String),
     #[display(fmt = "feature not implemented yet")]
@@ -46,15 +40,9 @@ pub enum ErrorKind {
 pub type Error = error::Error<ErrorKind>;
 
 pub fn run(args: Vec<String>) -> Result<(), Error> {
-    let frontend_names = {
-        let mut names = vec!["auto"];
-
-        names.extend(FRONTENDS.iter()
-            .map(|frontend| frontend.0));
-
-        names
-    };
-    let default_frontend = frontend_names[0];
+    let frontend_names = FRONTENDS.iter()
+            .map(|frontend| frontend.0)
+        .collect::<Vec<_>>();
 
     let backend_names = BACKENDS.iter()
         .map(|backend| backend.0)
@@ -64,22 +52,17 @@ pub fn run(args: Vec<String>) -> Result<(), Error> {
     let matches = App::new("HDLCompiler")
         .version("0.1")
         .author("Max Klein <max@maxkl.de>")
-        .arg(Arg::with_name("input_file_types")
-            .short("x")
+        .arg(Arg::with_name("input_file_type")
+            .short("t")
             .value_name("TYPE")
-            .help("Treat subsequent input files as having type TYPE")
-            .multiple(true)
-            .number_of_values(1)
+            .help("Treat the input file as having type TYPE. If not specified, the type is guessed based on the file extension")
             .possible_values(&frontend_names))
         .arg(Arg::with_name("dump_intermediate")
             .short("d")
-            .help("Treat all input files as intermediate code and dump them"))
+            .help("Treat the input file as intermediate code and pretty-print it"))
         .arg(Arg::with_name("frontend_only")
-            .short("c")
-            .help("Compile each input file separately to intermediate code"))
-        .arg(Arg::with_name("link_only")
-            .short("l")
-            .help("Compile the input files to intermediate code and link them, but don't run the backend"))
+            .short("i")
+            .help("Compile the input file to intermediate code"))
         .arg(Arg::with_name("backend")
             .short("b")
             .value_name("BACKEND")
@@ -92,18 +75,17 @@ pub fn run(args: Vec<String>) -> Result<(), Error> {
         .arg(Arg::with_name("backend_args")
             .short("B")
             .value_name("ARG")
-            .help("Pass argument ARG to the backend")
+            .help("Pass comma-separated list of arguments to the backend")
             .multiple(true)
             .require_delimiter(true)
             .require_equals(true))
         .arg(Arg::with_name("verbosity")
             .short("v")
             .multiple(true)
-            .help("Sets the level of verbosity"))
-        .arg(Arg::with_name("input_files")
+            .help("Increase logging verbosity"))
+        .arg(Arg::with_name("input_file")
             .value_name("FILE")
-            .help("The input files")
-            .multiple(true)
+            .help("The input file")
             .required(true))
         .get_matches_from(args);
 
@@ -113,46 +95,12 @@ pub fn run(args: Vec<String>) -> Result<(), Error> {
 
     let frontend_only = matches.is_present("frontend_only");
 
-    let link_only = matches.is_present("link_only");
-
     let backend_name = matches.value_of("backend").unwrap_or(default_backend);
 
     let output_file = matches.value_of("output_file");
 
-    let input_files = if let Some(input_files) = matches.values_of("input_files") {
-        let input_file_indices = matches.indices_of("input_files").unwrap();
-
-        let input_files_iter = input_files.zip(input_file_indices);
-
-        let input_types = if let Some(input_types) = matches.values_of("input_file_types") {
-            let input_type_indices = matches.indices_of("input_file_types").unwrap();
-
-            input_types.zip(input_type_indices)
-                .collect::<Vec<_>>()
-        } else {
-            Vec::new()
-        };
-
-        let input_files_with_type = input_files_iter
-            .map(|input_file| {
-                let next_index = input_types
-                    .binary_search_by_key(&input_file.1, |input_type| input_type.1)
-                    .unwrap_err();
-
-                let input_type = if next_index > 0 {
-                    input_types[next_index - 1].0
-                } else {
-                    default_frontend
-                };
-
-                (input_file.0, input_type)
-            })
-            .collect::<Vec<_>>();
-
-        input_files_with_type
-    } else {
-        Vec::new()
-    };
+    let input_file_name = matches.value_of("input_file").unwrap();
+    let input_file_type = matches.value_of("input_file_type");
 
     let backend_args = matches.values_of("backend_args")
         .map(|backend_args| backend_args.collect::<Vec<_>>())
@@ -163,59 +111,39 @@ pub fn run(args: Vec<String>) -> Result<(), Error> {
         return Err(ErrorKind::NotImplemented.into());
     }
 
-    if input_files.len() > 0 && frontend_only && output_file.is_some() {
-        return Err(ErrorKind::MultipleOutputsFileSpecified.into());
-    }
+    let input_path = Path::new(input_file_name);
 
-    let mut intermediate_files = Vec::new();
+    let frontend_name = if let Some(value) = input_file_type {
+        value
+    } else {
+        input_path.extension()
+            .and_then(OsStr::to_str)
+            .and_then(|ext| INPUT_FILE_TYPES.iter()
+                .find_map(|file_type| if file_type.0 == ext {
+                    Some(file_type.1)
+                } else {
+                    None
+                }))
+            .ok_or_else(|| ErrorKind::FileFormatNotRecognized(input_file_name.to_string()))?
+    };
 
-    for input_file in input_files {
-        let input_path = Path::new(input_file.0);
+    let frontend_fn = FRONTENDS.iter()
+        .find(|frontend| frontend.0 == frontend_name)
+        .unwrap().1;
 
-        let frontend_name = if input_file.1 == "auto" {
-            input_path.extension()
-                .and_then(OsStr::to_str)
-                .and_then(|ext| INPUT_FILE_TYPES.iter()
-                    .find_map(|file_type| if file_type.0 == ext {
-                        Some(file_type.1)
-                    } else {
-                        None
-                    }))
-                .ok_or_else(|| ErrorKind::FileFormatNotRecognized(input_file.0.to_string()))?
-        } else {
-            input_file.1
-        };
+    let intermediate = frontend_fn(input_path)
+        .map_err(|err| Error::with_source(ErrorKind::Compile(input_path.to_str().unwrap().to_string()), err))?;
 
-        let frontend_fn = FRONTENDS.iter()
-            .find(|frontend| frontend.0 == frontend_name)
+    if frontend_only {
+        // TODO: output intermediate file
+        return Err(ErrorKind::NotImplemented.into());
+    } else {
+        let backend_fn = BACKENDS.iter()
+            .find(|backend| backend.0 == backend_name)
             .unwrap().1;
 
-        let intermediate = frontend_fn(input_path)
-            .map_err(|err| Error::with_source(ErrorKind::Compile(input_path.to_str().unwrap().to_string()), err))?;
-
-        if frontend_only {
-            // TODO: output intermediate file
-            return Err(ErrorKind::NotImplemented.into());
-        } else {
-            intermediate_files.push(intermediate);
-        }
-    }
-
-    if !frontend_only {
-        let linked_intermediate = Linker::link(&intermediate_files)
-            .map_err(|err| Error::with_source(ErrorKind::Linker, err))?;
-
-        if link_only {
-            // TODO: output intermediate file
-            return Err(ErrorKind::NotImplemented.into());
-        } else {
-            let backend_fn = BACKENDS.iter()
-                .find(|backend| backend.0 == backend_name)
-                .unwrap().1;
-
-            backend_fn(output_file, &linked_intermediate, backend_args.as_slice())
-                .map_err(|err| Error::with_source(ErrorKind::Backend, err))?;
-        }
+        backend_fn(output_file, &intermediate, backend_args.as_slice())
+            .map_err(|err| Error::with_source(ErrorKind::Backend, err))?;
     }
 
     Ok(())
