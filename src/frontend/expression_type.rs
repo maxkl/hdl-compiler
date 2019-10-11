8e329a30ccce7419b8f8a680a88bf821f616f34f
maxkl <max@maxkl.de>
2019-10-11 20:03:40 +0200

    Parse and generate wires

--- src/frontend/expression_type.rs
+++ src/frontend/expression_type.rs
@@ -1,8 +1,31 @@
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum AccessType {
-    Read,
-    Write,
+pub struct AccessType {
+    pub readable: bool,
+    pub writable: bool,
+}
+
+impl AccessType {
+    pub fn r() -> AccessType {
+        AccessType {
+            readable: true,
+            writable: false,
+        }
+    }
+
+    pub fn w() -> AccessType {
+        AccessType {
+            readable: false,
+            writable: true,
+        }
+    }
+
+    pub fn rw() -> AccessType {
+        AccessType {
+            readable: true,
+            writable: true,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
